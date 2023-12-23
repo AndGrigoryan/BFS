83225dd2331d5cdd53ab9e2d07f6d8b9c6d37005
Andranik Grigoryan <andranik.grigoryan.02@gmail.com>
2023-12-23 21:08:30 +0400

    Add counter to calculate the length of the shortest path in the graph

--- BFS/BFS.cpp
+++ BFS/BFS.cpp
@@ -1,107 +1,115 @@
 #include <iostream>
 #include <unordered_map>
 #include <vector>
-#include <deque>
+#include <queue>
 #include <string>
 
-void AddToDequeFromVector
+void AddToQueueFromVector
 (
-    std::deque<char>& dq,
-    const std::vector<char>& vec,
-    const char& currentNode,
-    std::vector<char>& visited
+	std::queue<char>& q,
+	const std::vector<char>& vec,
+	const char& currentNode,
+	std::vector<char>& visited,
+	std::unordered_map<char, char>& parents
 )
 {
-    auto it = std::find(visited.begin(), visited.end(), currentNode);
 
-    if (it == visited.end())
-    {
-        visited.push_back(currentNode);
-        for (const auto value : vec)
-        {
-            dq.push_back(value);
-        }
-    }
+	for (const auto& value : vec)
+	{
+		auto it = std::find(visited.begin(), visited.end(), value);
+		if (it == visited.end())
+		{
+			parents[value] = currentNode;
+			q.push(value);
+			visited.push_back(value);
+		}
+	}
 }
 
-void ShowPath
+const void ShowPath
 (
-    std::unordered_map<char, char> parents,
-    char startNode,
-    char destinationNode
+	std::unordered_map<char, char>& parents,
+	char startNode,
+	char currentNode,
+	int& cnt
 )
 {
-    std::vector<char> path;
-    std::cout << "Path: ";
-    char currentNode = destinationNode;
-    
+	if (currentNode == startNode)
+	{
+		std::cout << currentNode;
+		return;
+	}
+	ShowPath(parents, startNode, parents[currentNode], cnt);
+	std::cout << " -> " << currentNode;
+	cnt++;
 }
 
 bool IsDestinationNode(const char& currentNode, const char& destinationNode)
 {
-    return currentNode == destinationNode;
+	return currentNode == destinationNode;
 }
 
 bool SearchQueue
 (
-    std::unordered_map<char, std::vector<char>> graph, 
-    char startNode, 
-    char destinationNode
+	std::unordered_map<char, std::vector<char>> graph,
+	char startNode,
+	char destinationNode
 )
 {
-    std::deque<char> dq;
-    std::vector<char> visited;
-    std::unordered_map<char, char> parents;
+	std::queue<char> q;
+	std::vector<char> visited;
+	std::unordered_map<char, char> parents;
 
-    AddToDequeFromVector(dq, graph[startNode], startNode, visited);
-    
+	q.push(startNode);
+	visited.push_back(startNode);
 
-    while (!dq.empty())
-    {
-        char currentNode = dq.front();
-        dq.pop_front();
-        if (IsDestinationNode(currentNode, destinationNode))
-        {
-            std::cout << "We have found the path to the destination!" << std::endl;
-            //ShowPath(parents, startNode, destinationNode);
-            return true;
-        }
-        else
-        {
-            AddToDequeFromVector(dq, graph[currentNode], currentNode, visited);
-        }
-    }
+	while (!q.empty())
+	{
+		char currentNode = q.front();
+		q.pop();
+		if (IsDestinationNode(currentNode, destinationNode))
+		{
+			std::cout << "We have found the path to the destination!" << std::endl;
+			int cnt = 0;
+			ShowPath(parents, startNode, destinationNode, cnt);
+			std::cout << " " << cnt << std::endl;
+			return true;
+		}
+		else
+		{
+			AddToQueueFromVector(q, graph[currentNode], currentNode, visited, parents);
+		}
+	}
 
-    std::cout << "We haven't found the path to the destination." << std::endl;
-    return false;
+	std::cout << "We haven't found the path to the destination." << std::endl;
+	return false;
 }
 
 int main()
 {
+	std::unordered_map <char, std::vector<char>> graph;
+	graph['a'] = { 'b' };
+	graph['b'] = { 'h', 'c' };
+	graph['h'] = { 'j', 'b' };
+	graph['c'] = { 'j', 'd','k' };
+	graph['j'] = { 'o', 'f','e', 'd', 'c', 'h' };
+	graph['k'] = { 'c', 'l' };
+	graph['f'] = { 'g', 'e' };
+	graph['e'] = { 'f', 'j', 'm', 'd' };
+	graph['d'] = { 'c', 'j', 'e', 'l' };
+	graph['l'] = { 'k', 'n', 'm' };
+	graph['m'] = { 'l', 'e' };
 
-    std::unordered_map <char, std::vector<char>> graph;
-    graph['a'] = { 'b' };
-    graph['b'] = { 'c', 'h' };
-    graph['h'] = { 'j', 'b'};
-    graph['c'] = { 'j', 'd','k'};
-    graph['j'] = { 'o', 'f','e', 'd', 'c', 'h'};
-    graph['k'] = { 'c', 'l'};
-    graph['f'] = { 'g', 'e'};
-    graph['e'] = { 'f', 'j', 'm', 'd'};
-    graph['d'] = { 'c', 'j', 'e', 'l'};
-    graph['l'] = { 'k', 'n', 'm'};
-    graph['m'] = { 'l', 'e'};
+	char startNode;
+	char destinationNode;
 
-    char startNode;
-    char destinationNode;
+	std::cout << "startNode: ";
+	std::cin >> startNode;
 
-    std::cout << "startNode: ";
-    std::cin >> startNode;
+	std::cout << "destinationNode: ";
+	std::cin >> destinationNode;
 
-    std::cout << "destinationNode: ";
-    std::cin >> destinationNode;
+	SearchQueue(graph, startNode, destinationNode);
 
-    SearchQueue(graph, startNode, destinationNode);
-
-    return 0;
+	return 0;
 }
